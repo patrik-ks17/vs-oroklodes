d2a4e1029f3c7e629d21858a86d6512a556f8bf6
KasaPatrik <kasaip02@gmail.com>
2022-10-24 10:08:41 +0200

    Add project files.

---
+++ peti002_oroklodes/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace peti002_oroklodes
+{
+    internal abstract class Allat 
+        // abstract: nem lehet osztályt létrehozni
+    {
+        protected string nev;
+        protected int kx, ky;
+        // Egyik konstruktor - csak a nevet tudjuk állítani
+        public Allat(string nev)
+        {
+            this.nev = nev;
+            this.kx = 0;
+            this.ky = 0;
+        }
+        // Másik konstruktor - a nevet és a koordinátát is be tudjuk állítani
+        public Allat(string nev, int x, int y) : this(nev)
+        {
+            this.kx = x;
+            this.ky = y;
+        }
+        public void mozog(int x, int y)
+        {
+            this.kx += x;
+            this.ky += y;
+        }
+    }
+
+    internal class Kutya : Allat
+    {
+        public Kutya(string neve) : base(neve)
+        {
+            this.nev += " kutya";
+        }
+        public Kutya(string neve, int x, int y) : base(neve, x, y)
+        {
+            this.nev += " kutya";
+            this.kx = x;
+            this.ky = y;
+        }
+        public void ugat() => Console.WriteLine("{0} ugat.", this.nev);
+    }
+    internal class Macska : Allat
+    {
+        public Macska(string neve) : base(neve)
+        {
+            nev += " cica";
+        }
+        public Macska(string neve, int x, int y) : base(neve, x, y)
+        {
+            nev += " cica";
+            kx = x;
+            ky = y;
+        }
+        public void nyavog()
+        {
+            Console.WriteLine($"{nev} nyávog.");
+        }
+    }
+    internal class Papagaj : Allat
+    {
+        protected int kz;
+        public Papagaj(string nev) : base(nev)
+        {
+            this.kz = 0;
+            this.nev += " papagáj";
+        }
+        public Papagaj(string nev, int x, int y, int z) : base(nev, x, y)
+        {
+            this.nev += " papagáj";
+            kx = x;
+            ky = y;
+            kz = z;
+        }
+        public virtual void mozog(int x, int y, int z)
+        {
+            mozog(x, y);
+            this.kz += z;
+            if (this.kz < 0)
+                this.kz = 0;
+        }
+    }
+
+    internal class Banyaszpapagaj : Papagaj
+    {
+        public Banyaszpapagaj(string nev) : base(nev) {}
+        public Banyaszpapagaj(string nev, int x, int y, int z) : base(nev, x, y, z) {}
+        public override void mozog(int x, int y, int z) // Papagaj mozog fugg felülirasa (csak virtualis fuggv-t)
+        {
+            mozog(x, y);
+            this.kz += z;
+        }
+    }
+
+}
---
+++ peti002_oroklodes/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using peti002_oroklodes;
+
+Allat a1 = new("Bimbó");
+Allat a2 = new("Antenna", 5, 13);
+
+a1.mozog(10, 8);
+a2.mozog(-4, 6);
+
+Kutya k1 = new("Bodri");
+Macska m1 = new("Cirmi", 4, 6);
+
+k1.ugat();
+m1.nyavog();
+
+Papagaj p1 = new("Pap");
+Banyaszpapagaj p3D = new("Peti", 6, 8, 2);
+
+p1.mozog(5, 8);
+p1.mozog(-1, 2, 4);
+
+p3D.mozog(-2, 2, -5);
+
+Console.ReadKey();
